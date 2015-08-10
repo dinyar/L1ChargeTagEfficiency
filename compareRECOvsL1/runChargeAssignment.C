657fc4079d495c3179b0ad4672b2a71a323c64bd
Dinyar Rabady <dinyar@rabady.at>
2015-08-10 11:40:25 +0200

    Fixed ntuple generator. Break occured after directory restructuring.

--- compareRECOvsL1/runChargeAssignment.C
+++ compareRECOvsL1/runChargeAssignment.C
@@ -1,5 +1,5 @@
 {
-    gROOT->ProcessLine(".x ../L1TriggerDPG/L1Ntuples/macros/initL1Analysis.C");
+    gROOT->ProcessLine(".x ../../L1TriggerDPG/L1Ntuples/macros/initL1Analysis.C");
     gROOT->ProcessLine(".L GMTChargeAssignmentNtupleizer.C+");
     gROOT->ProcessLine("GMTChargeAssignmentNtupleizer macro = GMTChargeAssignmentNtupleizer(\"ntuple_list\");");
     gROOT->ProcessLine("macro.run(-1);");
